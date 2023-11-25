8b8594d62cf4f88dc11745fb7355b46b9b1b993d
kcrutt <kevin.crutt.kc@gmail.com>
2023-11-25 16:31:02 -0300

    desafio feito

--- index.py
+++ index.py
@@ -1,3 +1,22 @@
-nome = "crutt14"
+nome = "Trevor"
+xp = 6543
+nivel = ""
 
-print("oi " + nome)
+if xp < 1000:
+    nivel = "Ferro"
+elif xp <= 2000:
+    nivel = "Bronze"
+elif xp <= 5000:
+    nivel = "Prata"
+elif xp <= 7000:
+    nivel = "Ouro"
+elif xp <= 8000:
+    nivel = "Platina"
+elif xp <= 9000:
+    nivel = "Ascendente"
+elif xp <= 10000:
+    nivel = "Imortal"
+elif xp >= 10001:
+    nivel = "Radiante"
+
+print("O Herói de nome " + nome + " está no nível de " + nivel)
